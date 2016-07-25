2f622338441fbaa9748476cec71c057c71224827
Raymond Chee <rjchee@gmail.com>
2016-07-25 02:25:14 -0700

    ran news_model through a linter

--- news_model.py
+++ news_model.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
+""" NewsModel
+This module contains logic to update a RandomWriter object from news headlines
+pulled from the internet.
+"""
 import argparse
-from collections import Counter
-import config_reader
 from html.parser import HTMLParser
 import os
 import pickle
+import re
+from urllib.parse import urlparse
+
 import psycopg2
+import requests
+
+import config_reader
 import randomwriter
 from randomwriter import RandomWriter
-import re
-import requests
-from urllib.parse import urlparse
 
 class NewsHTMLParser(HTMLParser):
     """ Parses HTML from a news website, pulling out headline text """
@@ -21,6 +26,7 @@ class NewsHTMLParser(HTMLParser):
         self.save_headline = save_headline
         self.is_headline_tag = is_headline_tag
         self.headline_count = 0
+        self.cur_headline = ""
         super().__init__()
 
 
@@ -47,23 +53,30 @@ class NewsHTMLParser(HTMLParser):
 
     @classmethod
     def identify_headline_class(cls, name):
+        """ Return a headline tag identifier based on the class attribute """
         def is_headline_tag(tag, attrs):
+            """ Identify a tag as one containing a headline by the class attribute """
             return ('class', name) in attrs
         return is_headline_tag
 
 
-class NewsModel:
+class NewsModel(object):
+    """ This class contains logic to access the PostgreSQL database on news models """
     def __init__(self):
         self.urlmap = {}
 
 
-    def update_model(self, writer, url, is_headline_tag, blacklist={}):
+    def update_model(self, writer, url, is_headline_tag, blacklist=None):
+        """ Update a single model with the URL and other parameters """
+        if blacklist is None:
+            blacklist = {}
         if url in self.urlmap:
             headlines, count = self.urlmap[url]
         else:
             headlines = []
-            endtime_re  = re.compile('^.*(?P<time>\\d\\d?:\\d{2} [AP]M \\wT)$')
+            endtime_re = re.compile('^.*(?P<time>\\d\\d?:\\d{2} [AP]M \\wT)$')
             def save_headline(headline):
+                """ Filter out useless headlines and store the useful ones for training later """
                 if "ttp://" in headline:
                     return False
                 endtime_match = endtime_re.match(headline)
@@ -76,8 +89,8 @@ class NewsModel:
                 blacklist[headline] = False
                 return ret
             html_parser = NewsHTMLParser(save_headline, is_headline_tag)
-            r = requests.get(url)
-            html_parser.feed(r.text)
+            html_page = requests.get(url)
+            html_parser.feed(html_page.text)
             count = html_parser.headline_count
             self.urlmap[url] = (headlines, count)
 
@@ -86,11 +99,13 @@ class NewsModel:
         return count
 
 
-    def get_db_conn(self):
+    @staticmethod
+    def get_db_conn():
+        """ Open a connection with the PostgreSQL database """
         url = urlparse(os.environ['DATABASE_URL'])
         return psycopg2.connect(
             database=url.path[1:],
-            user = url.username,
+            user=url.username,
             password=url.password,
             host=url.hostname,
             port=url.port
@@ -98,64 +113,78 @@ class NewsModel:
 
 
     def get_news_model(self, name, level=5, strategy=randomwriter.CharacterStrategy):
+        """ Retrieve a news model from the database """
         with self.get_db_conn() as conn:
             with conn.cursor() as cur:
                 # check if our table exists
-                cur.execute("SELECT EXISTS(SELECT * FROM information_schema.tables where table_name=%s);", ('models',))
+                cur.execute("SELECT EXISTS(SELECT * FROM information_schema.tables"
+                            + " where table_name=%s);", ('models',))
                 if not cur.fetchone()[0]:
                     # our table does not exist
-                    cur.execute("CREATE TABLE models (id serial PRIMARY KEY, name text NOT NULL UNIQUE, pickle bytea NOT NULL);")
+                    cur.execute("CREATE TABLE models (id serial PRIMARY KEY,"
+                                + " name text NOT NULL UNIQUE, pickle bytea NOT NULL);")
                 # get the pickle from the database
-                cur.execute("SELECT pickle FROM models WHERE name=%s;", (name,)) 
+                cur.execute("SELECT pickle FROM models WHERE name=%s;", (name,))
                 res = cur.fetchone()
                 if res:
                     res = res[0].tobytes()
-                    rw = RandomWriter.unpickle(res)
+                    random_writer = RandomWriter.unpickle(res)
                 else:
-                    rw = RandomWriter(level, strategy)
-                    pkl = pickle.dumps(rw)
+                    random_writer = RandomWriter(level, strategy)
+                    pkl = pickle.dumps(random_writer)
                     cur.execute("INSERT INTO models (name, pickle) VALUES (%s, %s);", (name, pkl))
-                return rw
+                return random_writer
 
 
     def delete_news_model(self, name):
+        """ Delete a news model from the PostgreSQL database """
         with self.get_db_conn() as conn:
             with conn.cursor() as cur:
                 cur.execute("DELETE FROM models WHERE name=%s;", (name,))
 
 
     def delete_headlines(self):
+        """ Clear the headlines table """
         with self.get_db_conn() as conn:
             with conn.cursor() as cur:
-                cur.execute("DROP TABLE IF EXISTS headlines;")
+                cur.execute("TRUNCATE headlines;")
 
 
     def update_news_models(self, models):
+        """ Update a list of models with headlines pulled from the websites in the config """
         news_sites = config_reader.read_configs()['NEWS']
 
         with self.get_db_conn() as conn:
             with conn.cursor() as cur:
                 # get blacklist table
-                cur.execute("SELECT EXISTS(SELECT * FROM information_schema.tables where table_name=%s);", ('headlines',))
+                cur.execute("SELECT EXISTS(SELECT * FROM information_schema.tables"
+                            + " where table_name=%s);", ('headlines',))
                 if not cur.fetchone()[0]:
                     # our table does not exist
-                    cur.execute("CREATE TABLE headlines (id serial PRIMARY KEY, headline text NOT NULL UNIQUE);")
+                    cur.execute("CREATE TABLE headlines (id serial PRIMARY KEY,"
+                                + " headline text NOT NULL UNIQUE);")
 
                 cur.execute("SELECT headline FROM headlines;")
-                last_headlines = cur.fetchall()
-                blacklist = dict.fromkeys((res[0] for res in last_headlines), True)
+                blacklist = dict.fromkeys((res[0] for res in cur), True)
 
         if isinstance(models, str):
             models = {models : None}
 
-        output = Counter()
+        output = {}
         for name, settings in models.items():
-            model = self.get_news_model(name) if settings is None else self.get_news_model(name, settings[0], settings[1])
+            if settings is None:
+                model = self.get_news_model(name)
+            else:
+                self.get_news_model(name, settings[0], settings[1])
             for url, class_name in news_sites.items():
-                output[url] += self.update_model(model, "http://" + url, NewsHTMLParser.identify_headline_class(class_name), blacklist)
+                # counts should never differ, so just save it each time
+                output[url] = self.update_model(model, "http://" + url,
+                                                NewsHTMLParser.identify_headline_class(class_name),
+                                                blacklist)
             with self.get_db_conn() as conn:
                 with conn.cursor() as cur:
-                    cur.execute("UPDATE models SET pickle=%s WHERE name=%s", (pickle.dumps(model), name))
+                    cur.execute("UPDATE models SET pickle=%s WHERE name=%s",
+                                (pickle.dumps(model), name))
 
         for url, count in output.items():
             print(url, count)
@@ -168,20 +197,22 @@ class NewsModel:
                         cur.execute("INSERT INTO headlines (headline) VALUES (%s);", (headline,))
 
 
-if __name__ == '__main__':
+def __main():
     config = config_reader.read_configs()
     models = {}
-    for name in config['MODEL_WEIGHTS']:
-        if name in config['CHARACTER_MODELS']:
-            models[name] = (int(config['CHARACTER_MODELS'][name]), randomwriter.CharacterStrategy)
-        elif name in config['WORD_MODELS']:
-            models[name] = (int(config['WORD_MODELS'][name]), randomwriter.WordStrategy)
+    for model_name in config['MODEL_WEIGHTS']:
+        if model_name in config['CHARACTER_MODELS']:
+            models[model_name] = (int(config['CHARACTER_MODELS'][model_name]),
+                                  randomwriter.CharacterStrategy)
+        elif model_name in config['WORD_MODELS']:
+            models[model_name] = (int(config['WORD_MODELS'][model_name]), randomwriter.WordStrategy)
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(dest='cmd')
     update_parser = subparsers.add_parser('update')
     update_parser.add_argument('-o', '--omit', default=[], nargs='+', choices=list(models.keys()))
     delete_parser = subparsers.add_parser('delete')
-    delete_parser.add_argument('models', nargs='*', choices=list(models.keys()) + [[]]) # no arguments means delete headlines only
+    # no arguments means delete headlines only
+    delete_parser.add_argument('models', nargs='*', choices=list(models.keys()) + [[]])
     parser.set_defaults(cmd='update', omit=[])
 
     args = parser.parse_args()
@@ -194,3 +225,7 @@ if __name__ == '__main__':
         for model in args.models:
             news_model.delete_news_model(model)
         news_model.delete_headlines()
+
+
+if __name__ == '__main__':
+    __main()
